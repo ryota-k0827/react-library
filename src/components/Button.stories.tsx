68c4933111ba4b0dad1e2399e226d25ee501b1f2
ryota-k0827 <ryota171124@icloud.com>
2022-06-03 05:18:01 +0900

    complete section3

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Button } from "./Button";
+import { Button, MarkButton } from "./Button";
 import { withKnobs, text, radios, select } from "@storybook/addon-knobs";
 
 export default {
@@ -30,3 +30,36 @@ export const Sample2: ComponentStory<typeof Button> = () => (
     onClick={() => {}}
   />
 );
+
+export const Sample3: ComponentStory<typeof Button> = (args) => (
+  <Button {...args} />
+);
+Sample3.args = {
+  label: text("label", "button"),
+  onClick: () => {},
+};
+Sample3.argTypes = {
+  // label: { control: { type: "array", separator: ["aaa", "bbb"] } },
+  bgColor: { control: "color" },
+  // bgColor: { control: { type: "object", options: colors } },
+  disable: { control: "boolean" },
+  // height: { control: { type: "number", min: 10, max: 200 } },
+  height: { control: { type: "range", min: 10, max: 200, step: 10 } },
+};
+
+const marks = {
+  default: { mark: "●", bgColor: "gray" },
+  blue: { mark: "♠︎", bgColor: "blue" },
+  red: { mark: "♥", bgColor: "red" },
+};
+export const Sample4: ComponentStory<typeof MarkButton> = (args) => (
+  <MarkButton {...args} />
+);
+Sample4.args = {
+  label: text("label", "button"),
+  onClick: () => {},
+};
+Sample4.argTypes = {
+  // label: { control: { type: "array", separator: ["aaa", "bbb"] } },
+  option: { control: { type: "radio", options: marks } },
+};
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,20 +4,61 @@ interface ButtonProps {
   label: string;
   onClick?: () => void;
   bgColor?: string;
+  disable?: boolean;
+  height?: number;
+}
+
+interface MarkButtonProps {
+  label: string;
+  onClick?: () => void;
+  height?: number;
+  option?: { mark: string; bgColor: string };
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ label, onClick, bgColor }: ButtonProps) => {
+export const Button = ({
+  label,
+  onClick,
+  bgColor,
+  disable,
+  height,
+}: ButtonProps) => {
   const buttonStyles = {
     color: "#fff",
     padding: "8px",
     borderRadius: "8px",
     backgroundColor: bgColor ? bgColor : "gray",
+    disable: disable ? disable : false,
+    height: height ? height : 60,
   };
   return (
     <button style={buttonStyles} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export const MarkButton = ({
+  label,
+  onClick,
+  height,
+  option = {
+    mark: "●",
+    bgColor: "gray",
+  },
+}: MarkButtonProps) => {
+  const buttonStyles = {
+    color: "#fff",
+    padding: "8px",
+    borderRadius: "8px",
+    backgroundColor: option.bgColor ? option.bgColor : "gray",
+    height: height + "px",
+  };
+  return (
+    <button style={buttonStyles} onClick={onClick}>
+      {option.mark}
       {label}
     </button>
   );
